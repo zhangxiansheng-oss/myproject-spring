7585debbd2823ee8d3e4c2a1a2bc196f937a72fa
“zbr” <1104790859@qq.com>
2026-01-05 11:22:07 +0800

    初始化

---
+++ src/main/java/org/zbr/myprojectSpring/MyprojectSpringApplication.java
@@ -0,0 +1,13 @@
+package org.zbr.myprojectSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyprojectSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyprojectSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/zbr/myprojectSpring/MyprojectSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zbr.myprojectSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyprojectSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
